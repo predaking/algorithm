1a47c10d0e7b7a42600c79e0731caa47c2876291
predaking <413255798@qq.com>
2021-09-14 19:29:08 +0800

    feat: quickSort

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -15,7 +15,47 @@ const quickSort = arr => {
 }
 
 /**
- * 时间复杂度：O(nlogn) 空间复杂度O(logn) 稳定排序
+ * 时间复杂度：O(nlogn) 空间复杂度O(logn) 不稳定排序
  */
+
+const partition = (arr, low, high) => {
+    const pivot = arr[low];
+
+    while (low < high) {
+        while (low < high && arr[high] > pivot) {
+            --high;
+        }
+        arr[low] = arr[high];
+
+        while (low < high && arr[low] <= pivot) {
+            ++low;
+        }
+        arr[high] = arr[low];
+    }
+
+    arr[low] = pivot;
+
+    return low;
+}
+
+const sort = (arr, low, high) => {
+    if (low < high) {
+        const pivot = partition(arr, low, high);
+
+        sort(arr, low, pivot - 1);
+        sort(arr, pivot + 1, high);
+    }
+
+    return arr;
+}
+
+/**
+ * @description 快速排序(教程版)
+ * @param {array} arr
+ */
+const quickSort = arr => {
+    sort(arr, 0, arr.length - 1);
+    return arr;
+}
 
 console.log(quickSort([2, -9, 0, 4, 3, 7, 9, 2, 2, 1, -4]));
