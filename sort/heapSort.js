fcb126cbb3847c6f5e7fe9df184e52c3de089990
predaking <413255798@qq.com>
2021-09-15 20:01:21 +0800

    fix: heapSort

--- sort/heapSort.js
+++ sort/heapSort.js
@@ -7,8 +7,18 @@ const heapSort = arr => {
         return arr;
     }
 
+    let len = arr.length;
+
     for (let i = Math.floor(arr.length / 2); i >= 0; --i) {
-        initHeap(arr, i);
+        initHeap(arr, i, len);
+    }
+
+    [arr[0], arr[len - 1]] = [arr[len - 1], arr[0]];
+    --len;
+
+    for (let i = len - 1; i > 0; --i) {
+        [arr[0], arr[i]]
+        initHeap(arr, i, len)
     }
 
     console.log(arr);
@@ -16,24 +26,25 @@ const heapSort = arr => {
     return arr;
 }
 
-const initHeap = (arr, i) => {
-    const len = arr.length;
+const initHeap = (arr, i, len) => {
     const left = 2 * i + 1;
     const right = 2 * i + 2;
     let largest = i;
 
     if (left < len && arr[left] > arr[i]) {
-        // [arr[left, arr[i]]] = [arr[i], arr[left]];
+        [arr[left], arr[i]] = [arr[i], arr[left]];
         largest = left;
     }
 
     if (right < len && arr[right] > arr[i]) {
-        // [arr[right, arr[i]]] = [arr[i], arr[right]];
+        [arr[right], arr[i]] = [arr[i], arr[right]];
         largest = right;
     }
 
+    console.log(arr[largest]);
+
     if (largest !== i) {
-        [arr[i], arr[largest]] = [arr[largest], arr[i]];
+        [arr[largest], arr[i]] = [arr[i], arr[largest]];
         initHeap(arr, largest);
     }
 }
