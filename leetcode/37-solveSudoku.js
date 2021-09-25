d7c9e3e6b5fc9ac12694e72f79eda82e252fb0f0
predaking <413255798@qq.com>
2021-09-26 00:53:32 +0800

    fix: 37-*

--- leetcode/37-solveSudoku.js
+++ leetcode/37-solveSudoku.js
@@ -1,20 +1,24 @@
 /**
+ * 编写一个程序，通过填充空格来解决数独问题。
+ *
+ * 数独的解法需 遵循如下规则：
+ *
+ * 数字 1-9 在每一行只能出现一次。
+ * 数字 1-9 在每一列只能出现一次。
+ * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。（请参考示例图）
+ * 数独部分空格内已填入了数字，空白格用 '.' 表示。
+ */
+
+/**
+ * @description 解数独
  * @param {character[][]} board
- * @return {void} Do not return anything, modify board in-place instead.
  */
 var solveSudoku = function(board) {
 
-	const memo = [];
 	const MAX = 9;
-	let lastRow = 0;
-	let lastCol = 0;
-
-	for (let val of board) {
-		memo.push([...val]);
-	}
 
 	// 校验当前填入值是否合法
-	const isValid = (board, row, col, cur) => {
+	const isValid = (row, col, cur) => {
 		const MAX = 9;
 
 		 for (let i = 0; i < MAX; ++i) {
@@ -43,74 +47,60 @@ var solveSudoku = function(board) {
 		 return true;
 	}
 
-	const backTrack = (row, col, val) => {
-		if (row === MAX) {
-			if (col === MAX) {
-				return board;
-			}
-
-			row--;
+	const backTrack = (row, col) => {
+		if (col === MAX) {
+			return backTrack(row + 1, 0);
 		}
 
-		if (col === MAX) {
-			return backTrack(row + 1, 0, 1);
+		if (row === MAX) {
+			return true;
 		}
 
 		if (board[row][col] !== '.') {
-			return backTrack(row, col + 1, val);
+			return backTrack(row, col + 1);
 		}
 
-		const res = isValid(board, row, col, val);
+		for (let i = 1; i <= MAX; ++i) {
+			if (!isValid(row, col, i)) {
+				continue;
+			}
 
-		if (res) {
-			board[row][col] = String(val);
-			console.log(JSON.stringify(board));
+			board[row][col] = String(i);
 
-			val = val === MAX ? 1 : val + 1;
-
-			backTrack(row, col + 1, val);
-		} else {
-			if (val === MAX) {
-				while (true) {
-					col--;
-					if (col < 0) {
-						col = MAX - 1;
-						row--;
-					}
-
-					if (memo[row][col] === '.') {
-						let tmp = board[row][col];
-						board[row][col] = '.';
-
-						if (tmp !== String(MAX)) {
-							backTrack(row, col, +tmp + 1);
-							break;
-						}
-					}
-				}
+			if (backTrack(row, col + 1)) {
+				return true;
 			} else {
-				backTrack(row, col, val + 1);
+				board[row][col] = '.';
 			}
 		}
 	}
 
-	backTrack(0, 0, 1);
+	backTrack(0, 0);
 
 	return board;
 };
 
-console.log(
-	solveSudoku(
-		[
-			["5","3",".",".","7",".",".",".","."],
-			["6",".",".","1","9","5",".",".","."],
-			[".","9","8",".",".",".",".","6","."],
-			["8",".",".",".","6",".",".",".","3"],
-			["4",".",".","8",".","3",".",".","1"],
-			["7",".",".",".","2",".",".",".","6"],
-			[".","6",".",".",".",".","2","8","."],
-			[".",".",".","4","1","9",".",".","5"],
-			[".",".",".",".","8",".",".","7","9"]
-		]
-	)
-);
+/**
+ * 测试用例：
+ * console.log(
+     isValidSudoku(
+         [
+             ["5","3",".",".","7",".",".",".","."],
+             ["6",".",".","1","9","5",".",".","."],
+             [".","9","8",".",".",".",".","6","."],
+             ["8",".",".",".","6",".",".",".","3"],
+             ["4",".",".","8",".","3",".",".","1"],
+             ["7",".",".",".","2",".",".",".","6"],
+             [".","6",".",".",".",".","2","8","."],
+             [".",".",".","4","1","9",".",".","5"],
+             [".",".",".",".","8",".",".","7","9"]
+         ]
+     )
+ );
+ */
+
+/**
+ * 本题核心： 回溯
+ *
+ * 反思：注意回溯时的条件
+ */
