18fb1da0205710fb1a991ee9b6356c809d738e02
predaking <413255798@qq.com>
2021-10-14 00:57:38 +0800

    deepClone

---
+++ tools/deepClone.js
@@ -0,0 +1,47 @@
+const deepClone = obj => {
+    if (typeof obj !== 'object') {
+        return obj;
+    }
+
+    const res = Array.isArray(obj) ? [] : {};
+
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            if (typeof obj[key] === 'object') {
+                if (obj[key] === null) {
+                    res[key] = obj[key];
+                } else {
+                    res[key] = deepClone(obj[key]);
+                }
+            } else {
+                res[key] = obj[key];
+            }
+        }
+    }
+
+    return res;
+}
+
+const obj = {
+    a: {
+        c: /a/,
+        d: undefined,
+        b: null
+    },
+    b: function () {
+        console.log(this.a)
+    },
+    c: [
+        {
+            a: 'c',
+            b: /b/,
+            c: undefined
+        },
+        'a',
+        3
+    ]
+}
+
+// obj.__proto__ = Object;
+
+console.log(deepClone(obj));
