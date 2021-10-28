1be196f5d61285301d230e47a2fd1a87889ab18f
predaking <413255798@qq.com>
2021-10-28 17:56:35 +0800

    fix: extend-*

--- other/extend.js
+++ other/extend.js
@@ -27,9 +27,10 @@ function Child1 (name) {
 	this.name = name;
 }
 
-Child1.prototype = new Parent('parent1', 50);
+// 注意： 以下两行顺序不同造成的结果不同，child1的构造函数不同
+const child1 = new Child1('child');
 
-const child = new Child1('child');
+Child1.prototype = new Parent('parent1', 50);
 
 /**
  * 二、借用构造函数继承
@@ -46,4 +47,39 @@ function Child2 (name) {
 
 const child2 = new Child2('child2');
 
-console.log(child2);
+/**
+ * 三、组合式继承
+ * @description 结合原型链继承与借用构造函数继承
+ * 优点： 可以传参到父类构造参数，可以继承父类的原型属性
+ * 缺点： 调用了两次父类构造函数
+ * @param {string} name
+ * @constructor Child3()
+ */
+function Child3 (name) {
+	Parent.call(this, 'parent3', 70);
+	this.name = name;
+}
+
+Child3.prototype = new Parent();
+
+const child3 = new Child3('child3');
+
+/**
+ * 四、原型式继承
+ * @description 函数中包含对象，返回函数的调用，属性随意添加
+ * 优点： 可以传参到父类构造参数，可以继承父类的原型属性
+ * 缺点： 调用了两次父类构造函数
+ * @param {string} name
+ * @constructor Child3()
+ */
+function Child4 (obj) {
+	function Fn() {}
+	Fn.prototype = obj;
+	return new Fn();
+}
+
+const child4 = Child4(new Parent('parent4', 90));
+
+child4.name = 'child4';
+
+console.log(child4.constructor.toString());
