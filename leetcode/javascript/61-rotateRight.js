8d1cde2b9922eef301c2389a8d4ec3044c39cac7
predaking <413255798@qq.com>
2024-02-18 01:24:49 +0800

    feat: 62-*

--- leetcode/javascript/61-rotateRight.js
+++ leetcode/javascript/61-rotateRight.js
@@ -14,5 +14,84 @@
  * @return {ListNode}
  */
 var rotateRight = function(head, k) {
+    var _head = head;
+    var index = 1;
+    var newHead = null;
+    var tail = null;
+    var len = 1;
 
+    if (!head || !head.next) {
+        return head;
+    }
+
+    while (_head.next) {
+        len++;
+        _head = _head.next
+    }
+
+    tail = _head;
+    _head = head;
+
+    var offset = len - k % len;
+
+    if (!(offset % len)) {
+        return head;
+    }
+
+    while (_head.next) {
+        if (index < offset) {
+            _head = _head.next;
+            index++;
+            continue;
+        }
+        if (!newHead) {
+            newHead = _head.next;
+            _head.next = null;
+            break;
+        }
+        _head = _head.next;
+    }
+
+    tail.next = head;
+
+    return newHead;
 };
+
+var node = null;
+
+node = {
+    val: 1,
+    next: {
+        val: 2,
+        next: {
+            val: 3,
+            next: {
+                val: 4,
+                next: {
+                    val: 5,
+                    next: null
+                }
+            }
+        }
+    }
+};
+
+node = {
+    val: 1,
+    next: {
+        val: 2,
+        next: null
+    }
+};
+
+/**
+ * 测试用例：
+ * console.log(rotateRight(node, 2));
+ */
+
+/**
+ * 本题核心：环形链表
+ *
+ * 反思：能想到的方法无论如何都绕不过两次循环，
+ * 第一次用来获取链表长度
+ */
