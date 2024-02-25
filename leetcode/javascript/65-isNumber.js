5ae7d9979a708b7ca554521480af891cca0a225a
predaking <413255798@qq.com>
2024-02-25 23:42:17 +0800

    feat: ltc-65-*

--- leetcode/javascript/65-isNumber.js
+++ leetcode/javascript/65-isNumber.js
@@ -32,25 +32,61 @@ var isNumber = function(s) {
     var hasDot = false;
     var hasE = false;
 
-    for (let i = 0; i < s.length; ++i) {
+    for (let i = 0; i < len; ++i) {
         var code = s[i].codePointAt();
-        if (i === 0) {
+
+        if (i === 0 && i + 1 < len) {
             if (s[0] === '+' || s[0] === '-') {
+                hasSign = true;
                 continue;
             }
         }
 
-        if (code >= 65 && code <= 97) {
+        if (code >= 48 && code <= 57) {
             continue;
         }
 
-        if (!hasDot && s[i] === '.') {
+        if (!hasDot && s[i] === '.' && !hasE && len > 1) {
+            hasDot = true; 
+            if (s[i - 1] === '+' || s[i - 1] === '-') {
+                if (
+                    i + 1 < len 
+                    && (s[i + 1].charCodeAt() >= 48 && s[i + 1].charCodeAt() <= 57)) {
+                    continue;
+                }
+            } else {
+                continue;
+            }
+        }
+
+        if (!hasSign && i + 1 < len && (s[i] === '+' || s[i] === '-') && (s[i - 1] === 'E' || s[i - 1] === 'e')) {
+            hasSign = true;
             continue;
         }
 
-        if (!hasE && (s[i] === 'E' || s[i] === 'e')) {
-            if (i + 1 < len) {
-                
+        if (!hasE && (s[i] === 'E' || s[i] === 'e') && i > 0) {
+            if (s[i - 1] === '+' || s[i - 1] === '-') {
+                res = false;
+                break;
+            }
+
+            if (s[i - 1] === '.' && i - 1 === 0) {
+                res = false;
+                break;
+            }
+
+            hasE = true;
+            hasSign = false;
+
+            if (
+                i + 1 < len 
+                    && (
+                        (s[i + 1].codePointAt() >= 48 && s[i + 1].codePointAt() <= 57) 
+                        || (s[i + 1] === '+' || s[i + 1] === '-') 
+                    )
+                    && (s[i - 1] !== '.' || (s[i - 1] === '.' && i - 1 > 0))
+            ) {
+                continue;
             }
         }
 
@@ -60,15 +96,43 @@ var isNumber = function(s) {
     return res;
 };
 
-console.log(isNumber('-123.456e789'));
-
 /**
  * 测试用例：
- * console.table(isNumber());
+ * console.log(isNumber('-123.456e789'));
+ * console.log(isNumber('0089'));
+ * console.log(isNumber('-0.1'));
+ * console.log(isNumber('+3.14'));
+ * console.log(isNumber('4.'));
+ * console.log(isNumber('-.9'));
+ * console.log(isNumber('2e10'));
+ * console.log(isNumber('-90E3'));
+ * console.log(isNumber('3e+7'));
+ * console.log(isNumber('+6e-1'));
+ * console.log(isNumber('53.5e93'));
+ * console.log(isNumber('46.e3'));
+
+ * console.log('------');
+
+ * console.log(isNumber('e+9+9'));
+ * console.log(isNumber('abc'));
+ * console.log(isNumber('1a'));
+ * console.log(isNumber('1e'));
+ * console.log(isNumber('e3'));
+ * console.log(isNumber('99e2.5'));
+ * console.log(isNumber('--6'));
+ * console.log(isNumber('-+3'));
+ * console.log(isNumber('95a54e53'));
+ * console.log(isNumber('.'));
+ * console.log(isNumber('.e1'));
+ * console.log(isNumber('6+1'));
+ * console.log(isNumber('+.'));
+ * console.log(isNumber('+'));
+ * console.log(isNumber('-e'));
+ * console.log(isNumber('+E3'));
  */
 
 /**
- * 本题核心：
+ * 本题核心：无
  *
- * 反思：
+ * 反思：毫无意义。。。
  */
