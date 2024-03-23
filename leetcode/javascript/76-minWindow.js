2d40b1bdf2430831eab0028a381d7dcfa8460cd5
predaking <413255798@qq.com>
2024-03-24 01:21:03 +0800

    feat: ltc-76-*

--- leetcode/javascript/76-minWindow.js
+++ leetcode/javascript/76-minWindow.js
@@ -16,17 +16,74 @@
  * @param {string} s
  * @param {string} t
  */
-var minWindow = function(s, t) {
+var minWindow = function (s, t) {
+    const sLen = s.length;
+    const tLen = t.length;
 
+    const sMap = new Map();
+    const tMap = new Map();
+
+    let minLen = sLen;
+    let res = '';
+
+    const isCover = () => [...tMap.entries()].every((item) => {
+        return sMap.get(item[0]) >= item[1];
+    });
+
+    for (let i = 0; i < tLen; ++i) {
+        const value = tMap.get(t[i]);
+        if (value) {
+            tMap.set(t[i], value + 1); 
+        } else {
+            tMap.set(t[i], 1);
+        }
+    }
+
+    let i = 0;
+    let j = tLen;
+    var str = s.slice(i, j);
+
+    for (let i = 0; i < str.length; ++i) {
+        const value = sMap.get(str[i]);
+        if (value) {
+            sMap.set(str[i], value + 1); 
+        } else {
+            sMap.set(str[i], 1);
+        }
+    }
+
+    var _isCover = false;
+
+    while (j <= sLen) {
+        _isCover = isCover();
+        if (_isCover) {
+            if (str.length <= minLen) {
+                minLen = str.length;
+                res = str;
+            }
+            var tmp = sMap.get(str[0]);
+            sMap.set(str[0], tmp - 1);
+            i++;
+            str = s.slice(i, j);
+        } else {
+            j++;
+            str = s.slice(i, j);
+            var tmp = (sMap.get(str[str.length - 1]) || 0);
+            sMap.set(str[str.length - 1], tmp + 1);
+        }
+    }
+
+    return res;
 };
 
 /**
  * 测试用例：
- * console.log(sortColors([2, 0, 2, 1, 1, 0]))
+ * console.log(minWindow('cabwefgewcwaefgcf', 'cae'));
  */
 
 /**
- * 本题核心：计数法
+ * 本题核心：滑动窗口、双指针
  *
- * 反思：dp[i][j] = dp[i - 1][j]
+ * 反思：用滑动窗口来解决该问题，现解法仍然耗时，主要在于 slice 方法的调用，以及
+ * 遍历 map 进行对比两个方面，更优化的办法可以参考官方用一个 map 存储的办法
  */
