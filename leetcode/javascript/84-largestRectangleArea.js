fe5e5587faf68a39b4a13061300f400a1bb35832
gongwei02 <gongwei02@xdfzx.com>
2024-04-25 17:45:57 +0800

    fix: ltc-84-*

---
+++ leetcode/javascript/84-largestRectangleArea.js
@@ -0,0 +1,27 @@
+/**
+ * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。
+ * 每个柱子彼此相邻，且宽度为 1 。
+ * 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+ */
+
+/**
+ * @description 柱状图中最大的矩形
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function (heights) {
+    
+};
+
+console.log(largestRectangleArea([2, 1, 5, 6, 2, 3]));
+
+/**
+ * 测试用例：
+ * console.log(largestRectangleArea([2, 1, 5, 6, 2, 3]));
+ */
+
+/**
+ * 本题核心：无
+ * 
+ * 反思：在head之前再追加一个节点写起来更容易理解
+ */
