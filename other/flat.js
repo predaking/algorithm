4a3283f9bc3b34c46ab05f1a780a9897a4f6ec44
gongwei02 <gongwei02@xdfzx.com>
2024-08-01 17:58:23 +0800

    feat: note

--- other/flat.js
+++ other/flat.js
@@ -19,4 +19,22 @@ var flatArray = function (arr) {
     return arr;
 }
 
-console.log(flatArray([1, 2, [3, 4, [6], 8, [9, [10, 11, [12, 13, 14]]]]]))
+var flatArray = function (depth = 1) {
+    let arr = Array.prototype.slice.call(this);
+    if (depth === 0) {
+        return arr;
+    }
+    return arr.reduce((pre, cur) => {
+        if (Array.isArray(cur)) {
+            return [...pre, ...flatArray.call(cur, depth - 1)];
+        } else {
+            return [...pre, cur];
+        }
+    }, []);
+}
+
+Array.prototype.flatArray = flatArray;
+
+const arr = [1, 2, [3, 4, [6], 8, [9, [10, 11, [12, 13, 14]]]]];
+
+console.log(arr.flatArray(Infinity));
